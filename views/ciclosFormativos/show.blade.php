d1440cf5463b913cee397a5e983d9168786892df
jlalbertm <160313677+jlalbertm@users.noreply.github.com>
2026-05-23 01:09:45 +0200

    Implement delete form for Ciclo Formativo
    Added a form for deleting the selected Ciclo Formativo.

--- views/ciclosFormativos/show.blade.php
+++ views/ciclosFormativos/show.blade.php
@@ -1,4 +1,4 @@
-<!-- En aquesta vista mostrarem les dades del cicle formatiu que hem seleccionat a la vista index-->
+<!-- En aquesta vista mostrarem les dades del cicle formatiu que hem seleccionat a la vista index, ens permet eliminar-lo-->
 
 @extends('template')
 @section('title','Dades del cicle formatiu')
@@ -9,5 +9,12 @@
     <p>Modalitat: {{$CiclosFormativo->modalidad}}</p>
     <p>Referència normativa del títol (BOE/DOGV): {{$CiclosFormativo->decreto_referencia}}</p>
     <p>Actiu: {{ $CiclosFormativo->activo ? 'SÍ' : 'NO' }}</p>
+
+                <!-- pasem el delete dins de un form per poder passar l'objecte, laravel donaba un error-->
+            <form action="{{ route('ciclosFormativos.destroy',$CiclosFormativo)}}" method="POST">
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="btn btn-danger">Borrar</button>
+            </form>
 
 @endsection
