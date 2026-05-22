7b6f3e46137595eff1a8a9aee1d28c0231a3c1e2
jlalbertm <160313677+jlalbertm@users.noreply.github.com>
2026-05-23 00:58:15 +0200

    Update comments from block to HTML style

--- views/ciclosFormativos/edit.blade.php
+++ views/ciclosFormativos/edit.blade.php
@@ -1,6 +1,5 @@
-/*
-Modul poer poder editar les dades d'un cicle.
-*/
+<!-- Modul poer poder editar les dades d'un cicle. -->
+
 @extends('template')
 @section('title','Formulari editar Cicle formatiu')
 @section('content')
@@ -9,17 +8,17 @@ Modul poer poder editar les dades d'un cicle.
 
         @method('PUT')
         @csrf
-        /*Nom del cicle formatiu*/
+        <!-- Nom del cicle formatiu -->
         <div class="form-group">
             <label for="nombre">Nom del cicle formatiu:</label>
             <input type="text" class="form-control" name="nombre" id="nombre" value="{{$CiclosFormativo->nombre}}">
         </div>
-        /*Família professional a la qual pertany*/
+        <!-- Família professional a la qual pertany -->
         <div class="form-group">
             <label for="familia_profesional">Família professional a la qual pertany:</label>
             <input type="text" class="form-control" name="familia_profesional" id="familia_profesional" value="{{$CiclosFormativo->familia_profesional}}">
         </div>
-        /*Grau, per evitar error affegim un select entre les 2 opcions que tenim*/
+        <!-- Grau, per evitar error affegim un select entre les 2 opcions que tenim -->
         <div class="form-group">
             <label for="price">Grau:</label>
             <select class="form-control" name="grado" id="grado">
@@ -28,7 +27,7 @@ Modul poer poder editar les dades d'un cicle.
             </select>
 
         </div>
-        /*Modaliat, per evitar error affegim un select entre les 2 opcions que tenim*/
+        <!-- Modaliat, per evitar error affegim un select entre les 2 opcions que tenim -->
         <div class="form-group">
             <label for="author">Modalitat:</label>
             <select class="form-control" name="modalidad" id="modalidad">
@@ -37,12 +36,12 @@ Modul poer poder editar les dades d'un cicle.
 
             </select>
         </div>
-        /*Referència normativa del títol */
+        <!-- Referència normativa del títol -->
         <div class="form-group">
             <label for="decreto_referencia">Referència normativa del títol (BOE/DOGV):</label>
             <input type="text" class="form-control" name="decreto_referencia" id="decreto_referencia" value="{{$CiclosFormativo->decreto_referencia}}">
         </div>
-        /*Actiu, este select encara que mostra un SI o No, al tindre un boolean a les taules guardem 1 o 0*/
+        <!-- Actiu, este select encara que mostra un SI o No, al tindre un boolean a les taules guardem 1 o 0 -->
         <div class="form-group">
             <label for="decreto_referencia">Actiu:</label>
             <select class="form-control" name="activo" id="activo">
