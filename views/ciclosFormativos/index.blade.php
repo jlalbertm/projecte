07e79fa43344f6e4b649227bc2870c92bc1da2ce
jlalbertm <160313677+jlalbertm@users.noreply.github.com>
2026-05-23 01:11:06 +0200

    Refactor edit form and delete button in index view

--- views/ciclosFormativos/index.blade.php
+++ views/ciclosFormativos/index.blade.php
@@ -1,4 +1,4 @@
-
+<!-- modul per mostrar la liista de ciclesformatius, ens permet editar o borrar un modul-->
 @extends('template')
 @section('title','Llista de Cicles Formatius')
 @section('content')
@@ -8,16 +8,14 @@
             <li><a href="{{ route('ciclosFormativos.show',$CiclosFormativo)}}">{{$CiclosFormativo->nombre}}
 
             </a>
+            <!-- pasem el delete dins de un form per poder passar l'objecte, laravel donaba un error-->
             <form action="{{ route('ciclosFormativos.destroy',$CiclosFormativo)}}" method="POST">
                 @method('DELETE')
                 @csrf
                 <button type="submit" class="btn btn-danger">Borrar</button>
             </form>
 
-            <!--<form action="{{ route('ciclosFormativos.edit', $CiclosFormativo) }}">
-                @csrf
-                <button type="submit" class="btn btn-warning">Editar</button>
-            </form>-->
+
             <a href="{{ route('ciclosFormativos.edit', $CiclosFormativo) }}" class="btn btn-warning">Editar</a>
 
             </li>
