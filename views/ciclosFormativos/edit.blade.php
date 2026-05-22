d01e781d3bfb7b4866b227f1f2cc2e8fc7e5f30b
jlalbertm <160313677+jlalbertm@users.noreply.github.com>
2026-05-23 00:53:03 +0200

    Add comments to form fields in edit view

--- views/ciclosFormativos/edit.blade.php
+++ views/ciclosFormativos/edit.blade.php
@@ -1,4 +1,6 @@
-
+/*
+Modul poer poder editar les dades d'un cicle.
+*/
 @extends('template')
 @section('title','Formulari editar Cicle formatiu')
 @section('content')
@@ -7,14 +9,17 @@
 
         @method('PUT')
         @csrf
+        /*Nom del cicle formatiu*/
         <div class="form-group">
             <label for="nombre">Nom del cicle formatiu:</label>
             <input type="text" class="form-control" name="nombre" id="nombre" value="{{$CiclosFormativo->nombre}}">
         </div>
+        /*Família professional a la qual pertany*/
         <div class="form-group">
             <label for="familia_profesional">Família professional a la qual pertany:</label>
             <input type="text" class="form-control" name="familia_profesional" id="familia_profesional" value="{{$CiclosFormativo->familia_profesional}}">
         </div>
+        /*Grau, per evitar error affegim un select entre les 2 opcions que tenim*/
         <div class="form-group">
             <label for="price">Grau:</label>
             <select class="form-control" name="grado" id="grado">
@@ -23,6 +28,7 @@
             </select>
 
         </div>
+        /*Modaliat, per evitar error affegim un select entre les 2 opcions que tenim*/
         <div class="form-group">
             <label for="author">Modalitat:</label>
             <select class="form-control" name="modalidad" id="modalidad">
@@ -31,10 +37,12 @@
 
             </select>
         </div>
+        /*Referència normativa del títol */
         <div class="form-group">
             <label for="decreto_referencia">Referència normativa del títol (BOE/DOGV):</label>
             <input type="text" class="form-control" name="decreto_referencia" id="decreto_referencia" value="{{$CiclosFormativo->decreto_referencia}}">
         </div>
+        /*Actiu, este select encara que mostra un SI o No, al tindre un boolean a les taules guardem 1 o 0*/
         <div class="form-group">
             <label for="decreto_referencia">Actiu:</label>
             <select class="form-control" name="activo" id="activo">
